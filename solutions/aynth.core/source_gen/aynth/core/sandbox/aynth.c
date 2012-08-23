d1e254c5ef5a6269a4900107bcc2c3066ea26600
aykutkilic <aykutkilic80@gmail.com>
2012-08-23 20:27:43 +0200

    added the signal sum operation which causes the problem we have.

---
+++ solutions/aynth.core/source_gen/aynth/core/sandbox/aynth.c
@@ -0,0 +1,19 @@
+#include "aynth.h"
+
+
+
+static void aynth_controlLoop(void) 
+{
+}
+
+
+static void aynth_sampleLoop(void) 
+{
+}
+
+
+
+
+<!TextGen not found for 'com.mbeddr.dataflow.structure.DataflowProcessor'!>
+
+
